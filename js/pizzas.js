ea0c0e0b9b598ec733e42296542eaf60e92cec0f
BMWe87 <bmwe87@fedora.home>
2024-11-04 08:54:45 +0100

    Changed whole interface conception by user's wish, added menu which is loaded dynamically

---
+++ js/pizzas.js
@@ -0,0 +1,34 @@
+class Pizza {
+	constructor(name, category, price) {
+		this.name = name;
+		(this.category = category), (this.price = price);
+	}
+}
+
+const menu = [
+    new Pizza('Margheritta', 'Wege', 33),
+    new Pizza('Mozza', 'Wege', 37),
+    new Pizza('Grilled Garden', 'Wege', 40),
+    new Pizza('Pesto (bez mozzarelli)', 'Wege', 40),
+    new Pizza('Popey', 'Wege', 40),
+    new Pizza('Quattro Formaggi', 'Wege', 41),
+    new Pizza('Greco (bianca, bez sosu)', 'Wege', 41),
+    new Pizza('Sweet Balsamico', 'Wege', 41),
+    new Pizza('Quattro Colori', 'Wege', 41),
+    new Pizza('Italian Lover', 'Z Mięskiem', 41),
+    new Pizza('Crudo', 'Z Mięskiem', 41),
+    new Pizza('Diesel Rider', 'Z Mięskiem', 41),
+    new Pizza('Capriciosa', 'Z Mięskiem', 41),
+    new Pizza('Hells Bells', 'Z Mięskiem', 41),
+    new Pizza('Jalapeno Honey', 'Z Mięskiem', 41),
+    new Pizza('Hot Honey', 'Z Mięskiem', 41),
+    new Pizza('Pancetta Funghi', 'Z Mięskiem', 41),
+    new Pizza('Pancetta Rosmarino', 'Z Mięskiem', 41),
+    new Pizza('Spianata Piccante', 'Z Mięskiem', 37),
+    new Pizza('Bambini Felici', 'Z Mięskiem', 41),
+    new Pizza('Costarica', 'Z Mięskiem', 41),
+    new Pizza('Tonno', 'Z Owocami Morza', 44),
+    new Pizza('Forrest Gump', 'Z Owocami Morza', 44),
+    new Pizza('Frutti di Mare (bez mozzarelli)', 'Z Owocami Morza', 41),
+    new Pizza('Alla Puttanesca (bez mozzarelli)', 'Z Owocami Morza', 40)
+];
--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
-const addBtn = document.querySelector('.controls-btn__add');
-const addMenu = document.querySelector('.add-menu');
-const itemsList = document.querySelector('.items-list')
-const selection = document.querySelector('#products-selection')
-const asa = document.querySelector('.asa')
+// import {Pizza, menu} from './pizzas.js';
+class Pizza {
+	constructor(name, category, price) {
+		this.name = name;
+		(this.category = category), (this.price = price);
+	}
+}
 
-const confirmPizzaButton = document.querySelector('.add-menu-button');
+const menu = [
+	new Pizza('Margheritta', 'Wege', 33),
+	new Pizza('Mozza', 'Wege', 37),
+	new Pizza('Grilled Garden', 'Wege', 40),
+	new Pizza('Pesto (bez mozzarelli)', 'Wege', 40),
+	new Pizza('Popey', 'Wege', 40),
+	new Pizza('Quattro Formaggi', 'Wege', 41),
+	new Pizza('Greco (bianca, bez sosu)', 'Wege', 41),
+	new Pizza('Sweet Balsamico', 'Wege', 41),
+	new Pizza('Quattro Colori', 'Wege', 41),
+	new Pizza('Italian Lover', 'Z Mięskiem', 41),
+	new Pizza('Crudo', 'Z Mięskiem', 41),
+	new Pizza('Diesel Rider', 'Z Mięskiem', 41),
+	new Pizza('Capriciosa', 'Z Mięskiem', 41),
+	new Pizza('Hells Bells', 'Z Mięskiem', 41),
+	new Pizza('Jalapeno Honey', 'Z Mięskiem', 41),
+	new Pizza('Hot Honey', 'Z Mięskiem', 41),
+	new Pizza('Pancetta Funghi', 'Z Mięskiem', 41),
+	new Pizza('Pancetta Rosmarino', 'Z Mięskiem', 41),
+	new Pizza('Spianata Piccante', 'Z Mięskiem', 37),
+	new Pizza('Bambini Felici', 'Z Mięskiem', 41),
+	new Pizza('Costarica', 'Z Mięskiem', 41),
+	new Pizza('Tonno', 'Z Owocami Morza', 44),
+	new Pizza('Forrest Gump', 'Z Owocami Morza', 44),
+	new Pizza('Frutti di Mare (bez mozzarelli)', 'Z Owocami Morza', 41),
+	new Pizza('Alla Puttanesca (bez mozzarelli)', 'Z Owocami Morza', 40),
+];
 
-addBtn.addEventListener('click', () => {
-	addMenu.classList.add('add-menu-visible');
+
+const pizzasPanel = document.querySelector('.panel');
+
+menu.forEach(pizza => {
+	const newPizza = document.createElement('div');
+	newPizza.classList.add('panel-item');
+	newPizza.textContent = `${pizza.name}`;
+	newPizza.setAttribute('data-price', `${pizza.price}`)
+	pizzasPanel.appendChild(newPizza);
 });
-
-confirmPizzaButton.addEventListener('click', () =>{
-    const newProduct = document.createElement('li');
-    newProduct.textContent = selection.value;
-    itemsList.appendChild(newProduct);
-    addMenu.classList.remove('add-menu-visible');
-})
